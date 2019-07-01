1eb325b26d12e053e31292c8e7ed625dedbfd768
karen-mtz <dkareen.mtz@gmail.com>
2019-07-01 11:44:44 -0500

    Updates

---
+++ precache-manifest.aefbf3aeab28872c2ec9f0e4c78b8eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cf7ac33c6dd75f857e316f1dbd3c24",
+    "url": "/Prueba-tecnica-Arkano/index.html"
+  },
+  {
+    "revision": "6274615c6c02483b6c25",
+    "url": "/Prueba-tecnica-Arkano/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "56202e89d17ef1b5ab03",
+    "url": "/Prueba-tecnica-Arkano/static/css/main.4a8790d3.chunk.css"
+  },
+  {
+    "revision": "6274615c6c02483b6c25",
+    "url": "/Prueba-tecnica-Arkano/static/js/2.e4322cce.chunk.js"
+  },
+  {
+    "revision": "56202e89d17ef1b5ab03",
+    "url": "/Prueba-tecnica-Arkano/static/js/main.6d8fcb8c.chunk.js"
+  },
+  {
+    "revision": "d6fe9389ed1647eaa65d",
+    "url": "/Prueba-tecnica-Arkano/static/js/runtime~main.2ae02a5b.js"
+  }
+]);
